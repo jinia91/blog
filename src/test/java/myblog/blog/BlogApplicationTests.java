26fc4b25a14eb82d6dafd985e94d8f7d7bea6de0
jinia91 <creee77@gmail.com>
2021-11-05 20:54:51 +0900

    211105 블로그 프로젝트 시작

---
+++ src/test/java/myblog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package myblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
