dc84961dba150afd365a3c0baab09a3bc12ef6db
jinia91 <creee77@gmail.com>
2021-12-16 20:38:38 +0900

    cicd

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$CURRENT_PID" ]; then
+        echo ">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다"
+else
+        echo ">kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $REPOSITORY/nohup.out 2>&1 &
+
