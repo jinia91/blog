b927f7609673bd127910ddf1ebc76087e14eab0c
jinia91 <creee77@gmail.com>
2021-12-16 20:49:00 +0900

    cicd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=springboot-blog
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl springboot-blog | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
 if [ -z "$CURRENT_PID" ]; then
         echo ">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
@@ -21,4 +35,3 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar $REPOSITORY/nohup.out 2>&1 &
-
