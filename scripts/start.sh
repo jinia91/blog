6d11c64a1150d6108f34d035b29f5126f16d43f2
jinia91 <creee77@gmail.com>
2021-12-17 02:17:42 +0900

    무중단 배포 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,4 +27,5 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-dev.yml \
+  -Dspning.profiles.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
