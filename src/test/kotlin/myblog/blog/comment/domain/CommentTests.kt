ec08a08d556a7fec22fa79b6fb271e09f99e8c3d
jinia91 <creee77@gmail.com>
2022-04-22 00:01:00 +0900

    테스트 코드 수정:

--- src/test/kotlin/myblog/blog/comment/domain/CommentTests.kt
+++ src/test/kotlin/myblog/blog/comment/domain/CommentTests.kt
@@ -1,5 +1,6 @@
 package myblog.blog.comment.domain
 
+import myblog.blog.comment.adapter.incomming.CommentBadRequestException
 import org.junit.jupiter.api.Test
 import java.lang.IllegalArgumentException
 import kotlin.test.assertFailsWith
@@ -8,7 +9,7 @@ class CommentTests {
 
     @Test
     fun `빈 댓글 작성시 에러`(){
-        assertFailsWith<IllegalArgumentException> {
+        assertFailsWith<CommentBadRequestException> {
             Comment.builder().build()
         }
     }
