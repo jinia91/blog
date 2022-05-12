62ac5ce43a35658a20563181f1d08c22e0a0732b
jinia91 <creee77@gmail.com>
2022-05-13 03:28:54 +0900

    캐시 기능 롤백

--- src/main/java/myblog/blog/BlogApplication.java
+++ src/main/java/myblog/blog/BlogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableCaching
+@EnableCaching
 public class BlogApplication {
 
 	public static void main(String[] args) {
