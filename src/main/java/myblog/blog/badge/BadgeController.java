63d570613ae75765294c95bc2fd2804bf2080c9a
Wonjin-Choi <creee77@gmail.com>
2022-06-03 21:05:27 +0900

    badge test

---
+++ src/main/java/myblog/blog/badge/BadgeController.java
@@ -0,0 +1,15 @@
+package myblog.blog.badge;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BadgeController {
+
+    @GetMapping("api/v1/badges")
+    String generateBadges() {
+        return "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\">" +"<circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\">" +"</svg>";
+    }
+}
