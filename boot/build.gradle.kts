0674c3f98a1e72abc5cfc5f6b6fb3e51288767e1
jinia91 <creee77@gmail.com>
2023-04-01 16:06:39 +0900

    add: bounded context based moudles

---
+++ boot/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.8.0"
+    val springBootVersion = "3.0.5"
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+}
+
+group = "Jinia's log"
+version = "2.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-Xjsr305=strict"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "Jinia's Log"
+
+include(
+    /**
+     * ################
+     * ## Jinia's Log##
+     * ################
+      */
+    "boot", // framework 의존성을 가진 main module
+    "article",
+    "user",
+    "comment",
+    "chat",
+    "notification",
+    "seo",
+)
