e865c9559fa371a3a892c2530cf9efb49f426464
jinia91 <creee77@gmail.com>
2023-05-07 01:51:13 +0900

    edit: article ddl

--- system/blog-core/adapter/persistence/core-rds/src/main/resources/db.migration.blogcore/V1__create_article_tbl.sql
+++ system/blog-core/adapter/persistence/core-rds/src/main/resources/db.migration.blogcore/V1__create_article_tbl.sql
@@ -4,13 +4,13 @@ CREATE TABLE article
     title         VARCHAR(50) NOT NULL,
     content       TEXT        NOT NULL,
     hit           BIGINT      NOT NULL DEFAULT 0,
-    thumbnail_url VARCHAR(255) NULL,
+    thumbnail_url VARCHAR(255)NOT NULL,
     writer_id     BIGINT      NOT NULL,
-    category_id   BIGINT NULL,
+    category_id   BIGINT      NOT NULL,
     status        VARCHAR(20) NOT NULL,
     created_date  DATETIME(6),
     updated_date  DATETIME(6),
     PRIMARY KEY (article_id)
 );
 
-CREATE FULLTEXT INDEX content_fts ON article (content);
+CREATE FULLTEXT INDEX title_content_fts ON article (title, content);
