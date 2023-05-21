6ef9b640a311dccd1a295cd34b8ad55f3b2ac4bb
jinia91 <creee77@gmail.com>
2023-05-21 15:20:43 +0900

    코드정리

--- boot/src/main/kotlin/kr/co/jiniaslog/App.kt
+++ boot/src/main/kotlin/kr/co/jiniaslog/App.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @ComponentScan(
     includeFilters = [
