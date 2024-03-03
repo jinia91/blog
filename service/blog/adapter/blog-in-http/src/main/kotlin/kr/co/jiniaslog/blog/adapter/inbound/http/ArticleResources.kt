e6a007b0510f9084b5e0799b928ffaebe2e35173
jinia91 <creee77@gmail.com>
2024-03-03 17:54:28 +0900

    impl blog api

--- service/blog/adapter/blog-in-http/src/main/kotlin/kr/co/jiniaslog/blog/adapter/inbound/http/ArticleResources.kt
+++ service/blog/adapter/blog-in-http/src/main/kotlin/kr/co/jiniaslog/blog/adapter/inbound/http/ArticleResources.kt
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
+import kr.co.jiniaslog.blog.domain.article.ArticleId
+import kr.co.jiniaslog.blog.usecase.IDeleteArticle
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import sun.jvm.hotspot.oops.CellTypeState.value
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -27,5 +32,30 @@ class ArticleResources(
         return ResponseEntity
             .created(URI("/api/v1/articles/${info.articleId.value}"))
             .body(ArticlePostResponse(info.articleId.value))
+    }
+
+    @PostMapping("/{articleId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun updateArticle(
+        @AuthUserId userId: Long?,
+        @RequestBody request: ArticleUpdateRequest,
+        @PathVariable articleId: Long,
+    ): ResponseEntity<ArticleUpdateResponse> {
+        require(userId != null) { "userId must not be null" }
+        val info = articleFacade.handle(request.toCommand(articleId))
+
+        return ResponseEntity
+            .ok(ArticleUpdateResponse(info.id.value))
+    }
+
+    @DeleteMapping("/{articleId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun deleteArticle(
+        @AuthUserId userId: Long?,
+        @PathVariable articleId: Long,
+    ): ResponseEntity<ArticleDeleteResponse> {
+        require(userId != null) { "userId must not be null" }
+        articleFacade.handle(IDeleteArticle.Command(ArticleId(articleId)))
+        return ResponseEntity.ok(ArticleDeleteResponse(articleId))
     }
 }
