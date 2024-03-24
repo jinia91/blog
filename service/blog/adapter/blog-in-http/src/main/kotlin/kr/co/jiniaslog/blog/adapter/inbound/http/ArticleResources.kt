4edc03d035232a5b137b89f5f0efbc917a2c6208
jinia91 <creee77@gmail.com>
2024-03-24 15:32:12 +0900

    build

--- service/blog/adapter/blog-in-http/src/main/kotlin/kr/co/jiniaslog/blog/adapter/inbound/http/ArticleResources.kt
+++ service/blog/adapter/blog-in-http/src/main/kotlin/kr/co/jiniaslog/blog/adapter/inbound/http/ArticleResources.kt
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import sun.jvm.hotspot.oops.CellTypeState.value
 import java.net.URI
 
 @RestController
