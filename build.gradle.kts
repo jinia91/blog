71cdd196831490f0e61c01275de6494c922060b7
jinia91 <creee77@gmail.com>
2024-06-24 17:07:40 +0900

    detekt 세팅 및 훅 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,38 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
+plugins {
+    id("io.gitlab.arturbosch.detekt") version "1.23.5"
+}
+
+tasks.withType<Detekt>().configureEach {
+    ignoreFailures = false
+    autoCorrect = true
+    reports {
+        html.required.set(true)
+    }
+    parallel = true
+    setSource(file(projectDir))
+    exclude(
+        "**/build/**",
+        "**/generated/**",
+        "**/resources/**",
+    )
+}
+
+repositories {
+    mavenCentral()
+}
+
+val detektVersion = "1.23.5"
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:$detektVersion")
+}
 
 tasks.register("importGitPreCommitHook", Copy::class) {
     val preCommitHookFile = file(".git/hooks/pre-commit")
     if (preCommitHookFile.exists()) {
-        println( "> \"${preCommitHookFile.absolutePath}\" 이 이미 존재합니다.")
+        println("> \"${preCommitHookFile.absolutePath}\" 이 이미 존재합니다.")
         println("> \"${preCommitHookFile.absolutePath}\" 을 파일을 삭제합니다.")
         preCommitHookFile.delete()
     }
@@ -15,7 +45,7 @@ tasks.register("importGitPreCommitHook", Copy::class) {
 tasks.register("importGitPrePushHook", Copy::class) {
     val prePushHookFile = file(".git/hooks/pre-push")
     if (prePushHookFile.exists()) {
-        println( "> \"${prePushHookFile.absolutePath}\" 이 이미 존재합니다.")
+        println("> \"${prePushHookFile.absolutePath}\" 이 이미 존재합니다.")
         println("> \"${prePushHookFile.absolutePath}\" 을 파일을 삭제합니다.")
         prePushHookFile.delete()
     }
