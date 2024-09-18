272cdd38a47601bde578c2d8d0d5113e01ce16c3
jinia91 <creee77@gmail.com>
2024-09-18 15:41:05 +0900

    옵저버빌리티 otel metric bridge

--- buildSrc/src/main/kotlin/plugin/observability.gradle.kts
+++ buildSrc/src/main/kotlin/plugin/observability.gradle.kts
@@ -11,5 +11,8 @@ dependencyManagement {
 }
 
 dependencies {
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.micrometer:micrometer-registry-otlp")
     implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
 }
---
+++ observability/docker-network.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker network create observability-network;
