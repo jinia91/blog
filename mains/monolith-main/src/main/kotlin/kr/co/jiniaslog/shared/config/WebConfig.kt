45705a45fe0a7ee137c726535c262649126f210f
jinia91 <creee77@gmail.com>
2024-09-18 14:52:04 +0900

    실행 sh 재설정

--- mains/monolith-main/src/main/kotlin/kr/co/jiniaslog/shared/config/WebConfig.kt
+++ mains/monolith-main/src/main/kotlin/kr/co/jiniaslog/shared/config/WebConfig.kt
@@ -1,19 +1,24 @@
 package kr.co.jiniaslog.shared.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl.maxAge
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+    @Value("\${host.domain}")
+    private lateinit var domain: String
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry
             .addMapping("/**")
             .allowCredentials(true)
             // fixme 주입하게하자
-            .allowedOrigins("https://www.jiniaslog.co.kr", "http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://$domain:3000", "https://$domain")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
             .maxAge(3600)
     }
 
