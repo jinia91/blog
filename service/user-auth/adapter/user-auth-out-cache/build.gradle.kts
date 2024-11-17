88257d08e1c7e3348d3342a397a51ba0b6144c7c
Jinia <creee77@gmail.com>
2024-11-17 17:46:37 +0900

    카페인 redis 로 마이그레이션  (#72)

--- service/user-auth/adapter/user-auth-out-cache/build.gradle.kts
+++ service/user-auth/adapter/user-auth-out-cache/build.gradle.kts
@@ -7,6 +7,6 @@ group = "kr.co.jiniaslog.user"
 dependencies {
     implementation(project(Modules.Service.AuthUser.Application.path))
     implementation(project(Modules.Libs.CoreKernel.path))
-    implementation(libs.spring.boot.starter.redisson)
+    implementation(libs.spring.data.redis)
     implementation(libs.caffeine)
 }
