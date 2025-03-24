ce81040937fc45d52e650a9690417d6add484b79
jinia91 <creee77@gmail.com>
2025-03-24 23:33:07 +0900

    배치 로그 추가

--- service/memo/adapter/memo-in-batch/src/main/kotlin/kr/co/jiniaslog/memo/adapter/inbound/batch/MemoBatchTask.kt
+++ service/memo/adapter/memo-in-batch/src/main/kotlin/kr/co/jiniaslog/memo/adapter/inbound/batch/MemoBatchTask.kt
@@ -5,12 +5,16 @@ import kr.co.jiniaslog.memo.usecase.IDeleteAllWithoutAdmin
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
+private val log = mu.KotlinLogging.logger {}
+
 @Component
 class MemoBatchTask(
     private val folderUseCasesFacade: FolderUseCasesFacade
 ) {
-    @Scheduled(cron = "0 0 0 * * MON") // 매주 월요일 0시 0분 0초
+    @Scheduled(cron = "0 0 0 * * MON", zone = "Asia/Seoul") // 매주 월요일 0시에 실행
     fun execute() {
+        log.info { "IDeleteAllWithoutAdmin 주기적 메모 삭제 실행" }
         folderUseCasesFacade.handle(IDeleteAllWithoutAdmin.Command())
+        log.info { "IDeleteAllWithoutAdmin 주기적 메모 삭제 완료" }
     }
 }
