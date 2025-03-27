acb81a91ab8d667ed1afff6dbeb6ed34216ea3d6
jinia91 <creee77@gmail.com>
2025-03-27 20:18:43 +0900

    테스트 코드 추가

--- mains/monolith-main/src/test/kotlin/kr/co/jiniaslog/blog/queries/IGetTopNQueryTests.kt
+++ mains/monolith-main/src/test/kotlin/kr/co/jiniaslog/blog/queries/IGetTopNQueryTests.kt
@@ -1,7 +1,11 @@
 package kr.co.jiniaslog.blog.queries
 
+import io.kotest.matchers.shouldBe
 import jakarta.persistence.EntityManager
 import kr.co.jiniaslog.TestContainerAbstractSkeleton
+import kr.co.jiniaslog.blog.domain.ArticleTestFixtures
+import kr.co.jiniaslog.blog.domain.tag.Tag
+import kr.co.jiniaslog.blog.domain.tag.TagName
 import kr.co.jiniaslog.blog.outbound.ArticleRepository
 import kr.co.jiniaslog.blog.outbound.TagRepository
 import kr.co.jiniaslog.blog.usecase.tag.IGetTopNTags
@@ -22,6 +26,24 @@ class IGetTopNQueryTests : TestContainerAbstractSkeleton() {
     lateinit var em: EntityManager
 
     @Test
-    fun `없는 태그의 이름이 주어지면 게시글에 추가할 수 있다`() {
+    fun `태그 사용이 많은 순으로 조회할 수 있다`() {
+        // given
+        val tag1 = tagRepository.save(Tag.newOne(TagName("tag1")))
+        val tag2 = tagRepository.save(Tag.newOne(TagName("tag2")))
+        val tag3 = tagRepository.save(Tag.newOne(TagName("tag3")))
+        val tag4 = tagRepository.save(Tag.newOne(TagName("tag4")))
+        val tag5 = tagRepository.save(Tag.newOne(TagName("tag5")))
+
+        articleRepository.save(ArticleTestFixtures.createPublishedArticle(tags = listOf(tag1)))
+        articleRepository.save(ArticleTestFixtures.createPublishedArticle(tags = listOf(tag1, tag3)))
+        articleRepository.save(ArticleTestFixtures.createPublishedArticle(tags = listOf(tag1, tag2, tag3)))
+        articleRepository.save(ArticleTestFixtures.createPublishedArticle(tags = listOf(tag1, tag2, tag3, tag4, tag5)))
+
+        // when
+        val result = sut.handle(IGetTopNTags.Query(3))
+
+        // then
+        result.tags.size shouldBe 3
+        result.tags.keys shouldBe setOf(tag1.entityId, tag3.entityId, tag2.entityId)
     }
 }
