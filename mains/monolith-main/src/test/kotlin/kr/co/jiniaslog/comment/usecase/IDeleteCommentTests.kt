9fbe29dc88d3aa58cfbb231245b5394716a965cc
jinia91 <creee77@gmail.com>
2025-04-08 19:12:58 +0900

    테스트 검증 추가

--- mains/monolith-main/src/test/kotlin/kr/co/jiniaslog/comment/usecase/IDeleteCommentTests.kt
+++ mains/monolith-main/src/test/kotlin/kr/co/jiniaslog/comment/usecase/IDeleteCommentTests.kt
@@ -1,35 +1,47 @@
-// package kr.co.jiniaslog.comment.usecase
-//
-// import io.kotest.assertions.withClue
-// import kr.co.jiniaslog.TestContainerAbstractSkeleton
-// import kr.co.jiniaslog.comment.domain.CommentTestFixtures
-// import kr.co.jiniaslog.comment.outbound.CommentRepository
-// import org.junit.jupiter.api.Test
-// import org.springframework.beans.factory.annotation.Autowired
-//
-// class IDeleteCommentTests : TestContainerAbstractSkeleton() {
-//    @Autowired
-//    private lateinit var sut : IDeleteComment
-//
-//    @Autowired
-//    private lateinit var commentRepository: CommentRepository
-//
-//    @Test
-//    fun `익명 유저는 패스워드가 일치하면 자신의 댓글을 삭제할 수 있다` () {
-//        // given
-//        val asIsComment = withClue("익명 유저 기존 댓글이 존재한다") {
-//            commentRepository.save(
-//                CommentTestFixtures.createAnonymousComment()
-//            )
-//        }
-//        val command = IDeleteComment.Command(
-//            asIsComment.entityId,
-//            null,
-//            asIsComment
-//        )
-//
-//        // when
-//
-//        sut.handle()
-//    }
-// }
+package kr.co.jiniaslog.comment.usecase
+
+import io.kotest.assertions.withClue
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import kr.co.jiniaslog.TestContainerAbstractSkeleton
+import kr.co.jiniaslog.comment.domain.Comment
+import kr.co.jiniaslog.comment.domain.CommentTestFixtures
+import kr.co.jiniaslog.comment.outbound.CommentRepository
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class IDeleteCommentTests : TestContainerAbstractSkeleton() {
+    @Autowired
+    private lateinit var sut: IDeleteComment
+
+    @Autowired
+    private lateinit var commentRepository: CommentRepository
+
+    @Test
+    fun `익명 유저는 패스워드가 일치하면 자신의 댓글을 삭제할 수 있다`() {
+        // given
+        val password = "password"
+
+        val asIsComment = withClue("익명 유저 기존 댓글이 존재한다") {
+            commentRepository.save(
+                CommentTestFixtures.createAnonymousComment(
+                    userPassword = password
+                )
+            )
+        }
+
+        val command = IDeleteComment.Command(
+            asIsComment.entityId,
+            null,
+            password
+        )
+
+        // when
+        val result = sut.handle(command)
+
+        // then
+        val foundComment = commentRepository.findById(asIsComment.entityId)
+        foundComment.shouldNotBeNull()
+        foundComment.status shouldBe Comment.Status.DELETED
+    }
+}
